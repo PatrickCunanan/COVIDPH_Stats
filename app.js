ba92b0b0b8ad5de8816be83ae4639eb021b2277b
Cunanan <patrick.b.f.cunanan@accenture.com>
2020-03-30 16:35:21 +0800

    added the data to the webpage

--- app.js
+++ app.js
@@ -23,8 +23,19 @@ app.get("/", function(req, res) {
   req.end(function(response) {
     if (response.error) throw new Error(response.error);
 
-    const stats = response.body.data;
-    res.render('index');
+    var statsstring = JSON.stringify(response.body.data.covid19Stats);
+    var stats = JSON.parse(statsstring);
+    var confirmed = stats[0].confirmed;
+    var deaths = stats[0].deaths;
+    var recovered = stats[0].recovered;
+    var lastUpdate = stats[0].lastUpdate;
+    console.log(stats, confirmed, deaths, recovered, lastUpdate);
+    res.render("index", {
+      confirmed: confirmed,
+      deaths: deaths,
+      recovered: recovered,
+      lastUpdate: lastUpdate
+    });
   });
 });
 
